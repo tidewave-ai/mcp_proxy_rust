e979408f2f0d4f3cc0f4fae444aa9bac12d93499
Steffen Deusch <steffen@deusch.me>
2025-09-01 18:35:59 +0200

    update sdk to latest

--- tests/echo/mod.rs
+++ tests/echo/mod.rs
@@ -1,20 +1,33 @@
 use rmcp::{
-    ServerHandler,
-    model::{ServerCapabilities, ServerInfo},
-    schemars, tool,
+    ErrorData as McpError,
+    handler::server::{router::tool::ToolRouter, wrapper::Parameters},
+    model::*,
+    tool, tool_handler, tool_router,
 };
+
 #[derive(Debug, Clone, Default)]
-pub struct Echo;
-#[tool(tool_box)]
+pub struct Echo {
+    tool_router: ToolRouter<Echo>,
+}
+#[tool_router]
 impl Echo {
+    #[allow(dead_code)]
+    pub fn new() -> Self {
+        Self {
+            tool_router: Self::tool_router(),
+        }
+    }
+
     #[tool(description = "Echo a message")]
-    fn echo(&self, #[tool(param)] message: String) -> String {
-        message
+    fn echo(&self, Parameters(object): Parameters<JsonObject>) -> Result<CallToolResult, McpError> {
+        Ok(CallToolResult::success(vec![Content::text(
+            serde_json::Value::Object(object).to_string(),
+        )]))
     }
 }
 
-#[tool(tool_box)]
-impl ServerHandler for Echo {
+#[tool_handler]
+impl rmcp::ServerHandler for Echo {
     fn get_info(&self) -> ServerInfo {
         ServerInfo {
             instructions: Some("A simple echo server".into()),
